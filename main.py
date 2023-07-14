2b2cf2087fd3a3adab19a313e8bb0203b5405f33
Kunal Mallick <kunalmallicks3@gmail.com>
2023-07-14 18:31:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import pickle
 import pandas as pd
 import streamlit as st
-#from sklearn.preprocessing import StandardScaler
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline, make_pipeline
 
 #st.set_page_config(layout="wide")
 
